be433abc64cd87bc944beca53e3642c20cc4602d
Corey Andrews <candrew@localhost.localdomain>
2020-12-10 06:09:35 -0500

    Day 5 complete

---
+++ day05/day5_1.hs
@@ -0,0 +1,21 @@
+-- |
+
+seatID :: String -> Int
+seatID path = calcSeatNum (getRowSeat path (0,0) rowRange seatRange)
+  where
+    rowRange = 64
+    seatRange = 4
+    getRowSeat (x:xs) (row,seat) rr sr
+      | x == 'F' = getRowSeat xs (row,seat) (rr `div` 2) sr
+      | x == 'B' = getRowSeat xs (row+rr,seat) (rr `div` 2) sr
+      | x == 'L' = getRowSeat xs (row,seat) rr (sr `div` 2)
+      | x == 'R' = getRowSeat xs (row,seat+sr) rr (sr `div` 2)
+      | otherwise = (row,seat)
+    getRowSeat [] rs _ _ = rs
+    calcSeatNum (x, y) = 8 * x + y
+
+main :: IO ()
+main = do
+  contents <- getContents
+  let bPasses = lines contents
+  putStrLn (show (foldl max 0 (map seatID bPasses)))
